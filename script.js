638176f08d77a430938b25c9a434759b23db9537
DEV VRAT <devvrat709@gmail.com>
2022-01-25 22:30:25 +0530

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Assingment 1 - Number is greater than 10
+
+var number = 11
+if(number>=10) console.log("Greater than 10")
+else if(number===10) console.log("Equal to 10")
+else console.log("Less than 10")
+
+// Assignment 2- Find the greatest number among three numbers.
+var number1 = 1154
+var number2 = 200
+var number3 = 304
+
+if(number1 > number2 && number1> number3) console.log(number1 + " is greatest number")
+if(number2 > number1 && number2> number3) console.log(number2 + " is greatest number")
+if(number3 > number1 && number3> number2) console.log(number3 + " is greatest number")
+
+
+
